0c9544b9ee22a75341a68f3c6d99f5cc82aa0e37
daadaadaah <daadaadaah@gmail.com>
2020-08-18 03:58:42 +0900

    Add test code

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+var serviceAccount = require("./config/devConfig.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const express = require('express');
+const app = express();
+const db = admin.firestore();
+
+const cors = require('cors');
+app.use( cors( { origin: true }));
+
+// Routes
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!1');
+})
+
+app.post('/user', (req, res) => {
+
+  (async () => {
+    try {
+      await db.collection('user').doc('/'+req.body.uid+'/')
+      .create({
+          uid: req.body.uid,
+          email: req.body.email,
+          photoURL: req.body.photoURL
+      })
+      return res.status(200).send();
+
+    } catch(error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+
+  })();
+
+})
+
+// Export the API to Firebase Cloud Functions
+exports.api = functions.https.onRequest(app);
