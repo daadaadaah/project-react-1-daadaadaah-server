97d483595ec2e09cdfbf8fbf429fcd06e44f3ec2
daadaadaah <daadaadaah@gmail.com>
2020-08-18 17:18:10 +0900

    remove config

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-var serviceAccount = require("./config/devConfig.json");
+var serviceAccount = require("./devConfig.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
