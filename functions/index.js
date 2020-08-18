34fde62db771b0a1cd1fb85bf4da4902da345888
daadaadaah <daadaadaah@gmail.com>
2020-08-18 19:15:50 +0900

    Update initializeApp

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,18 @@ const admin = require('firebase-admin');
 var serviceAccount = require("./devConfig.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(...serviceAccount,
-    {
-      private_key: serviceAccount.private_key.replace(/\\n/g, '\n')
-    }   
+  credential: admin.credential.cert({
+    "type": serviceAccount.type,
+    "project_id": serviceAccount.project_id,
+    "private_key_id": serviceAccount.private_key_id,
+    "private_key": serviceAccount.private_key.replace(/\\n/g, '\n'),
+    "client_email": serviceAccount.client_email,
+    "client_id": serviceAccount.client_id,
+    "auth_uri": serviceAccount.auth_uri,
+    "token_uri": serviceAccount.token_uri,
+    "auth_provider_x509_cert_url": serviceAccount.auth_provider_x509_cert_url,
+    "client_x509_cert_url": serviceAccount.client_x509_cert_url  
+  }  
 )});
 
 const express = require('express');
